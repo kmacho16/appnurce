97ec7fe3cae26266408fbf7382eb1852e9af7479
Juan Camacho <jccamfer@gmail.com>
2017-08-06 16:07:25 -0500

    floats

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', 'db4free.net'),
+            'port' => env('DB_PORT', '3307'),
+            'database' => env('DB_DATABASE', 'nurce_app'),
+            'username' => env('DB_USERNAME', 'nurce_admin'),
+            'password' => env('DB_PASSWORD', 'camacho16'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
